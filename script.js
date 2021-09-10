6be131d4daecaea3b87c6b3678be09004fe87d62
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2021-09-10 12:57:56 +0300

    Add data attributes to cells for track which cell was clicked and add to game board object correct index of clicked cell, add reset button. Add css styles for cell border. Add event listeners to the cells, add proper sign after cells were clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let gameBoard = {
-    board: [],
+    board: ["","","",
+            "","","",
+            "","",""],
 }
 
 let displayController
@@ -7,4 +9,26 @@ let displayController
 const userFactory = (name) => {
     this.score = 0;
     return {name, score}
+}
+let count = 0;
+
+const btns = document.querySelectorAll('.cell');
+
+btns.forEach(item => item.addEventListener('click',() =>{
+    if(item.textContent === ""){
+    if(count === 0){
+    
+    item.textContent = "X"
+    count++
+    gameBoard.board[Number(item.dataset.num)] = item.textContent 
+    }else{
+    item.textContent = "O"
+    count--
+    gameBoard.board[Number(item.dataset.num)] = item.textContent    
+}}}))
+
+document.querySelector('.reset').addEventListener('click', () => btns.forEach(item => item.textContent = ""))
+
+for(let i = 0; i <=8;i++){
+    btns[i].textContent = gameBoard.board[i]
 }
