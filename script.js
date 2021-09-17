2d0924ec7e026829ebec969d2e5f42bd3d67b96f
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2021-09-17 15:00:18 +0300

    Add in html 2 new modal forms. One for new game other one is for end game scenarios. Add functionality to this froms in JS. Add event listeners for buttons in this form. Add css style for main game board

--- script.js
+++ script.js
@@ -4,12 +4,95 @@ let gameBoard = {
             "","",""],
 }
 
+
+const newGameForm = document.querySelector('.new-game');
+const endGameForm = document.querySelector('.end-game');
+const newRoundBtn = document.querySelector('.new-round-btn');
+const newGameBtn = document.querySelector('.new-game-btn');
+const checkMark = "&#10003";
+const crossMark = "&#215";
+
+function toggleHidden(block){
+    block.classList.toggle('hidden');
+}
+
+
+function newGame(){
+    resetGame();
+    newGameForm.classList.remove('hidden')
+}
+
+function checkResult(){
+    if(gameBoard.board[0] === 'X' && gameBoard.board[1] === 'X' && gameBoard.board[2] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[3] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[5] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[6] === 'X' && gameBoard.board[7] === 'X' && gameBoard.board[8] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[0] === 'X' && gameBoard.board[3] === 'X' && gameBoard.board[6] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[1] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[7] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[2] === 'X' && gameBoard.board[5] === 'X' && gameBoard.board[8] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[0] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[8] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[2] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[6] === 'X'){
+        endGameForm.classList.remove('hidden')
+    }
+    //
+    if(gameBoard.board[0] === 'O' && gameBoard.board[1] === 'O' && gameBoard.board[2] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[3] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[5] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[6] === 'O' && gameBoard.board[7] === 'O' && gameBoard.board[8] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[0] === 'O' && gameBoard.board[3] === 'O' && gameBoard.board[6] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[1] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[7] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[2] === 'O' && gameBoard.board[5] === 'O' && gameBoard.board[8] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[0] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[8] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+    if(gameBoard.board[2] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[6] === 'O'){
+        endGameForm.classList.remove('hidden')
+    }
+
+}
+
+function resetGame(){
+    gameBoard.board = ["","","","","","","","",""];
+    drawGameFromObject();
+}
+
+function drawGameFromObject(){
+    for(let i = 0; i <=8;i++){
+        btns[i].textContent = gameBoard.board[i]
+    }
+}
+
 let displayController
 
 const userFactory = (name) => {
     this.score = 0;
     return {name, score}
 }
+
 let count = 0;
 
 const btns = document.querySelectorAll('.cell');
@@ -17,18 +100,73 @@ const btns = document.querySelectorAll('.cell');
 btns.forEach(item => item.addEventListener('click',() =>{
     if(item.textContent === ""){
     if(count === 0){
-    
     item.textContent = "X"
     count++
-    gameBoard.board[Number(item.dataset.num)] = item.textContent 
+    gameBoard.board[Number(item.dataset.num)] = item.textContent
+    checkResult(); 
     }else{
     item.textContent = "O"
     count--
-    gameBoard.board[Number(item.dataset.num)] = item.textContent    
+    gameBoard.board[Number(item.dataset.num)] = item.textContent
+    checkResult();     
 }}}))
 
-document.querySelector('.reset').addEventListener('click', () => btns.forEach(item => item.textContent = ""))
 
-for(let i = 0; i <=8;i++){
-    btns[i].textContent = gameBoard.board[i]
-}
+document.querySelector('#gameMode').addEventListener('change',()=> {
+    if(event.target.value === "Human"){
+        document.querySelector('#AIlvl').remove()
+        let label = document.createElement('label');
+        let input = document.createElement('input');
+        let p = document.createElement('p');
+        let br = document.createElement('br');
+        p.textContent = "What's your name, Player 2? ";
+        input.size = 10;
+        label.appendChild(p);
+        label.appendChild(input);
+        label.appendChild(br);
+        label.id = 'humanAdd'
+        document.querySelector('form').insertBefore(label,document.querySelector('#start'))
+    }else if(event.target.value === "AI"){
+        document.querySelector('#humanAdd').remove()
+        let label = document.createElement('label');
+        let select = document.createElement('select');
+        let option1 = document.createElement('option');
+        let option2 = document.createElement('option');
+        let option3 = document.createElement('option');
+        let p = document.createElement('p');
+        let br = document.createElement('br');
+        option1.value = "easy";
+        option1.textContent = "Easy";
+        option2.value = "medium";
+        option2.textContent = "Medium";
+        option3.value = "hard";
+        option3.textContent = "Hard";
+        p.textContent = "Choose AI level";
+        select.appendChild(option1);
+        select.appendChild(option2);
+        select.appendChild(option3);
+        label.appendChild(p);
+        label.appendChild(select);
+        label.appendChild(br);
+        label.id = "AIlvl"
+        document.querySelector('form').insertBefore(label,document.querySelector('#start'))
+    }
+})
+
+document.querySelector('#start').addEventListener('click',(e) => {
+    e.preventDefault();
+    toggleHidden(newGameForm);
+})
+
+newGameBtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    toggleHidden(endGameForm);
+    resetGame();
+    toggleHidden(newGameForm)
+})
+
+newRoundBtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    toggleHidden(endGameForm)
+    resetGame()
+})
